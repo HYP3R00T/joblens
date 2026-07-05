e8457f92d921922f930eff01ac423383a9544784
Rajesh <hyperoot.tech@proton.me>
2026-07-05 22:57:05 +0000

    chore: cleanup

--- src/env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="astro/client" />
-
-declare module '@fontsource-variable/inter'
-declare module '@fontsource-variable/jetbrains-mono'
-declare module '@fontsource-variable/space-grotesk'
